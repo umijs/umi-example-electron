eae80bc80cf4c10524f7726c0b99411a9d14496b
chencheng <sorrycc@gmail.com>
2018-06-06 09:09:44 +0800

    support import electron, node's native modules and app's deps in renderer

--- src/renderer/.umirc.js
+++ src/renderer/.umirc.js
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import slash from 'slash';
 
 export default {
   disableServiceWorker: true,
@@ -11,4 +12,26 @@ export default {
       immer: true,
     }],
   ],
+  externals(context, request, callback) {
+    const isDev = process.env.NODE_ENV === 'development';
+    let isExternal = false;
+    const load = [
+      'electron',
+      'fs',
+      'path',
+      'os',
+      'url',
+      'child_process'
+    ];
+    if (load.includes(request)) {
+      isExternal = `require("${request}")`;
+    }
+    const appDeps = Object.keys(require('../../app/package').dependencies);
+    if (appDeps.includes(request)) {
+      const orininalPath = slash(join(__dirname, '../../app/node_modules', request));
+      const requireAbsolute = `require('${orininalPath}')`;
+      isExternal = isDev ? requireAbsolute : `require('${request}')`;
+    }
+    callback(null, isExternal);
+  },
 };
--- src/renderer/pages/index.js
+++ src/renderer/pages/index.js
@@ -1,4 +1,5 @@
 import { connect } from 'dva';
+import is from 'electron-is';
 
 export default connect(state => ({
   g: state.g,
@@ -6,6 +7,7 @@ export default connect(state => ({
   <div style={{ textAlign: 'center' }}>
     <h1>Yay! Welcome to umi and dva!</h1>
     <h2>Data Test: {props.g}</h2>
+    <h2>is.osx(): {JSON.stringify(is.osx())}</h2>
     <br /><br />
     <img src={require('../assets/yay.jpg')} width="400" />
   </div>
